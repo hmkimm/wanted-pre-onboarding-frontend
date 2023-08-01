6e29315b55796a00fa7251670218f791a21f76cb
hmkimm <lisamin7@naver.com>
2023-08-02 03:45:33 +0900

    Feat: 회원가입 페이지 구현

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+import Input from "../Components/Input";
+import Button from "../Components/Button";
+
+import SignUpAPI from "../Utils/SignUpAPI";
+
+const SignUp = () => {
+  const navigate = useNavigate();
+  const [isValidSignUp, setIsValidSignUp] = useState(false);
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await SignUpAPI(inputs);
+    console.log(response);
+    navigate("/signin");
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    setIsValidSignUp(inputs.email.includes("@") && inputs.password.length >= 8);
+  }, [inputs]);
+
+  return (
+    <SignUpLayout onSubmit={handleSubmit}>
+      <Input
+        name="email"
+        value={inputs.email}
+        onChange={handleInputChange}
+        data-testid="email-input"
+        placeholder="이메일"
+      />
+      <Input
+        name="password"
+        value={inputs.password}
+        onChange={handleInputChange}
+        data-testid="password-input"
+        placeholder="비밀번호"
+        $mb="50px"
+      />
+      <Button data-testid="signup-button" disabled={!isValidSignUp}>
+        회원가입
+      </Button>
+    </SignUpLayout>
+  );
+};
+
+const SignUpLayout = styled.form`
+  width: 550px;
+  height: 352px;
+  margin: 100px auto;
+  padding: 35px;
+  box-sizing: border-box;
+  border: 1px solid var(--light-gray);
+  border-radius: 10px;
+`;
+export default SignUp;
