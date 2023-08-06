37b845b4480e059bd77b863c68ab5d696bfa2b24
hmkimm <lisamin7@naver.com>
2023-08-06 22:26:28 +0900

    Chore: 배포 테스트

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter, HashRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <Router>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </Router>
+  </BrowserRouter>
 );
