029417c1d12294e005494d184873a34e5d05eae3
hmkimm <lisamin7@naver.com>
2023-08-06 21:05:06 +0900

    Feat: HashRouter로 변경

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 );
